5b714d74d5c5fbfaef0a001fb4526cb8a8c9025c
zhfushengx2048 <53218036+zhfushengx2048@users.noreply.github.com>
2020-03-19 18:00:00 +0800

    create master

---
+++ src/main/java/xyz/fusheng/version/VersionApplication.java
@@ -0,0 +1,18 @@
+package xyz.fusheng.version;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+/**
+ * @author 25610
+ * @Data: 这是主支 2020/3/19/ 17:58
+ */
+@SpringBootApplication
+public class VersionApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(VersionApplication.class, args);
+    }
+
+}
