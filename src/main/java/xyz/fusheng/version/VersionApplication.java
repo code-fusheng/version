f56786dc5d638cf3ff65158c3bf762c1901456d5
zhfushengx2048 <53218036+zhfushengx2048@users.noreply.github.com>
2020-03-19 18:05:09 +0800

    分支 version2 提交变更

--- src/main/java/xyz/fusheng/version/VersionApplication.java
+++ src/main/java/xyz/fusheng/version/VersionApplication.java
@@ -8,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 /**
  * @author 25610
  * @Data: 这是主支 2020/3/19/ 17:58
+ * @Data: 这是分支1 versions 2020/3/19 18:04
  */
 @SpringBootApplication
 public class VersionApplication {
