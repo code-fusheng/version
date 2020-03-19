69ad28cfb05b30583e848d9f577a141ebc4de207
zhfushengx2048 <53218036+zhfushengx2048@users.noreply.github.com>
2020-03-19 18:12:05 +0800

    分支 version3 提交变更

--- src/main/java/xyz/fusheng/version/VersionApplication.java
+++ src/main/java/xyz/fusheng/version/VersionApplication.java
@@ -9,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author 25610
  * @Data: 这是主支 2020/3/19/ 17:58
  * @Data: 这是分支 version3 2020/3/19/ 18:07
+ * @Data: 这是分支 version3 2020/3/19/ 18:07
  */
 @SpringBootApplication
 public class VersionApplication {
